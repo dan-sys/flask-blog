d7a84de5b3dced02a5cdda4cfdb482cd2f13c568
dan-sys <adeniyidaniel3@gmail.com>
2024-07-23 06:16:39 +0200

    first commit : learning flask,jinja, html templates setup

---
+++ app.py
@@ -0,0 +1,119 @@
+from flask import Flask, render_template
+
+
+
+#create flask app instance
+app = Flask(__name__)
+
+
+
+#create a route decorator
+@app.route('/')
+
+def index():
+    first_name = "Dominus"
+    stuff = "this is a bold text"
+    fav_piazza = ["Romana","Galiani","Pax",560]
+    return render_template("index.html",
+                           first_name=first_name, 
+                           stuff=stuff,
+                           fav_piazza=fav_piazza)
+
+# localhost:5000/user/Chronus
+@app.route('/user/<name>')
+
+def user(name):
+    return render_template("user.html",user_name=name)
+
+# create custom error pages
+
+#invalid url 
+@app.errorhandler(404)
+
+def page_not_found(err):
+    return render_template("404.html"),404
+
+@app.errorhandler(500)
+
+def page_not_found(err):
+    return render_template("500.html"),500
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
